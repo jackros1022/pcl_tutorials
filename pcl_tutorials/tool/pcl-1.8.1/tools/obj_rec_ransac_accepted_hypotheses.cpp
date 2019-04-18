3f477a455b029af2af7930b220559b52da081e10
王冲 <jackros1022@users.noreply.github.com>
2019-04-18 16:12:27 +0800

    init

--- pcl_tutorials/tool/pcl-1.8.1/tools/obj_rec_ransac_accepted_hypotheses.cpp
+++ pcl_tutorials/tool/pcl-1.8.1/tools/obj_rec_ransac_accepted_hypotheses.cpp
@@ -1,37 +1,3 @@
-/*
- * Software License Agreement (BSD License)
- *
- *  Copyright (c) 2011, Willow Garage, Inc.
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of the copyright holder(s) nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
- */
 
 /*
  * obj_rec_ransac_accepted_hypotheses.cpp
@@ -62,6 +28,14 @@
 #include <algorithm>
 #include <cstdio>
 #include <vector>
+
+/************************************************************************/
+// error£ºno override found for vtkpolydatamapper
+#include "vtkAutoInit.h" 
+VTK_MODULE_INIT(vtkRenderingOpenGL); // VTK was built with vtkRenderingOpenGL2
+VTK_MODULE_INIT(vtkInteractionStyle);
+
+/************************************************************************/
 
 using namespace std;
 using namespace pcl;
@@ -417,12 +391,12 @@ run (float pair_width, float voxel_size, float max_coplanarity_angle, int num_hy
   PointCloud<Normal>::Ptr scene_normals (new PointCloud<Normal> ()), model_normals (new PointCloud<Normal> ());
 
   // Get the points and normals from the scene
-  if ( !vtk_to_pointcloud ("../../test/tum_table_scene.vtk", *scene_points, *scene_normals) )
+  if ( !vtk_to_pointcloud ("./test/tum_table_scene.vtk", *scene_points, *scene_normals) )
     return;
 
   vtkPolyData *vtk_model = vtkPolyData::New ();
   // Get the points and normals from the scene
-  if ( !vtk_to_pointcloud ("../../test/tum_amicelli_box.vtk", *model_points, *model_normals, vtk_model) )
+  if ( !vtk_to_pointcloud ("./test/tum_amicelli_box.vtk", *model_points, *model_normals, vtk_model) )
     return;
 
   // The recognition object
